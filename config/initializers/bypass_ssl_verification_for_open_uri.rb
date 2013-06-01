cf5c272c746931346177e7c175a48b8d0266d336
stonefruit <jeromeng888@gmail.com>
2013-06-01 21:53:19 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/bypass_ssl_verification_for_open_uri.rb
@@ -0,0 +1,2 @@
+require 'OpenSSL'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GainBlog::Application.config.secret_token = 'ecba31a0d5e3d7fcd1180940f6d4d9da177c8ff51a3f1d895650177f43b1e08bd964d557bbcb827af1096f8a2f245650ca9fad3cbafc17c536f09949ff2de996'
